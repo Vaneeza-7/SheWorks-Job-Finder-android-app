a68fb619ac8a70b037beb06c9a602530fed2a132
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-29 23:42:41 +0500

    first commit: main page and login

---
+++ app/src/main/java/com/vaneezaahmad/sheworks/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.vaneezaahmad.sheworks
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tap = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.main)
+        tap.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
